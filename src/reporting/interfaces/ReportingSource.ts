217c524122af1c5abba9f49887def6859f41677b
Andreas Riemer <mail@andreasriemer.com>
2021-10-07 16:37:45 +0200

    make query var any

--- src/reporting/interfaces/ReportingSource.ts
+++ src/reporting/interfaces/ReportingSource.ts
@@ -1,5 +1,5 @@
 export interface ReportingSource<T extends Record<string, unknown>> {
   key: string;
   label: string;
-  dataCallback: (variables?: Record<string, unknown>) => Promise<Array<T> | undefined> | undefined;
+  dataCallback: (variables?: any) => Promise<Array<T> | undefined> | undefined;
 }
